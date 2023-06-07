6d2be9fce332787c395d7f8bca1a4b1d74cbe6ca
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-07 11:16:45 +0700

    Percabangan | If Then and Switch With out Break

---
+++ src/com/dicoding/javafundamental/percabangan/IfThen.java
@@ -0,0 +1,14 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class IfThen {
+    public static void main(String[] args) {
+        boolean isOn = true;
+
+        //void ganti() {
+            if (isOn) {
+                System.out.println("Menyalakan lampu");
+                System.out.println("Menyalakan lampu laim");
+            }
+        //}
+    }
+}
